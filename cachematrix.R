0fb2be1d6229a40ed8f2775e3029fe2fa1168645
ic7y <wutao08@qq.com>
2014-06-21 21:09:07 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,10 +4,11 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-        
+        # use det() to decide  whether a matrix is inverstible
         if( det(x) == 0 ){
                 stop("the matrix is irreversible ")
         }
+        # cache matrix
         mtrx<<- x
         getmatrix<-function(){
                 mtrx
@@ -20,14 +21,17 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        # get matrix which been cached befroe 
         mtrx<-x$getmatrix()
         if( det(mtrx) ==0 ){
                 stop("the matrix is irreversible ")
    
         }else{
+        ## compute a matrix that is the inverse of 'x'
                 message("compute the cache matrix's inverse")
                 invers<-solve( mtrx )
         }
+        
         invers
 }
